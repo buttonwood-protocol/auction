b5fcee2630e4766b5ff718d127f43c94e4baa73d
SocksNFlops <socks&flops@prl.one>
2022-09-14 00:44:16 -0700

    fixing Michal's PR

--- src/interfaces/IAuctionFactory.sol
+++ src/interfaces/IAuctionFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
+import {IDualAuction} from "./IDualAuction.sol";
+
 /**
  * @notice Interface for Auction factory contracts
  */
@@ -32,5 +34,5 @@ interface IAuctionFactory {
         uint256 maxPrice,
         uint256 tickWidth,
         uint256 endDate
-    ) external returns (address);
+    ) external returns (IDualAuction);
 }
